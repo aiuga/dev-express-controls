f1575cf7c3591913315fa338414085fb9118a203
aiuga <ady_iuga@yahoo.com>
2019-11-28 23:41:54 +0200

    data grid api

--- src/app/controls/data-grid2/data-grid2.component.ts
+++ src/app/controls/data-grid2/data-grid2.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
-// import * as AspNetData from "devextreme-aspnet-data-nojquery";
+import * as AspNetData from "devextreme-aspnet-data-nojquery";
 
 @Component({
   selector: 'app-data-grid2',
@@ -10,57 +10,57 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class DataGrid2Component implements OnInit {
 
-  // customersData: any;
-  // shippersData: any;
-  // dataSource: any;
-  // url: string;
-  // masterDetailDataSource: any;
+  customersData: any;
+  shippersData: any;
+  dataSource: any;
+  url: string;
+  masterDetailDataSource: any;
 
 
-  // constructor() {
-  //   this.url = "https://js.devexpress.com/Demos/Mvc/api/DataGridWebApi";
+  constructor() {
+    this.url = "https://js.devexpress.com/Demos/Mvc/api/DataGridWebApi";
 
-  //   this.dataSource = AspNetData.createStore({
-  //     key: "OrderID",
-  //     loadUrl: this.url + "/Orders",
-  //     insertUrl: this.url + "/InsertOrder",
-  //     updateUrl: this.url + "/UpdateOrder",
-  //     deleteUrl: this.url + "/DeleteOrder",
-  //     onBeforeSend: function (method, ajaxOptions) {
-  //       ajaxOptions.xhrFields = { withCredentials: true };
-  //     }
-  //   });
+    this.dataSource = AspNetData.createStore({
+      key: "OrderID",
+      loadUrl: this.url + "/Orders",
+      insertUrl: this.url + "/InsertOrder",
+      updateUrl: this.url + "/UpdateOrder",
+      deleteUrl: this.url + "/DeleteOrder",
+      onBeforeSend: function (method, ajaxOptions) {
+        ajaxOptions.xhrFields = { withCredentials: true };
+      }
+    });
 
-  //   this.customersData = AspNetData.createStore({
-  //     key: "Value",
-  //     loadUrl: this.url + "/CustomersLookup",
-  //     onBeforeSend: function (method, ajaxOptions) {
-  //       ajaxOptions.xhrFields = { withCredentials: true };
-  //     }
-  //   });
+    this.customersData = AspNetData.createStore({
+      key: "Value",
+      loadUrl: this.url + "/CustomersLookup",
+      onBeforeSend: function (method, ajaxOptions) {
+        ajaxOptions.xhrFields = { withCredentials: true };
+      }
+    });
 
-  //   this.shippersData = AspNetData.createStore({
-  //     key: "Value",
-  //     loadUrl: this.url + "/ShippersLookup",
-  //     onBeforeSend: function (method, ajaxOptions) {
-  //       ajaxOptions.xhrFields = { withCredentials: true };
-  //     }
-  //   });
-  // }
-
-  ngOnInit(){
-    
+    this.shippersData = AspNetData.createStore({
+      key: "Value",
+      loadUrl: this.url + "/ShippersLookup",
+      onBeforeSend: function (method, ajaxOptions) {
+        ajaxOptions.xhrFields = { withCredentials: true };
+      }
+    });
   }
 
-  // getMasterDetailGridDataSource(id: number): any {
-  //   return {
-  //     store: AspNetData.createStore({
-  //       loadUrl: this.url + '/OrderDetails',
-  //       loadParams: { orderID: id },
-  //       onBeforeSend: function (method, ajaxOptions) {
-  //         ajaxOptions.xhrFields = { withCredentials: true };
-  //       }
-  //     })
-  //   };
-  // }
+  ngOnInit() {
+
+  }
+
+  getMasterDetailGridDataSource(id: number): any {
+    return {
+      store: AspNetData.createStore({
+        loadUrl: this.url + '/OrderDetails',
+        loadParams: { orderID: id },
+        onBeforeSend: function (method, ajaxOptions) {
+          ajaxOptions.xhrFields = { withCredentials: true };
+        }
+      })
+    };
+  }
 }
